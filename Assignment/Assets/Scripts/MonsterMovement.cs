8755b2b3ee2802aff7da8163f572dd5a9f4541bd
Yung729 <leongkahyung@gmail.com>
2023-09-15 11:55:55 +0800

    x

--- Assignment/Assets/Scripts/MonsterMovement.cs
+++ Assignment/Assets/Scripts/MonsterMovement.cs
@@ -20,7 +20,7 @@ public class MonsterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isChasing)
+/*        if (isChasing)
         {
             if (transform.position.x > playerTransform.position.x)
             {
@@ -57,6 +57,26 @@ public class MonsterMovement : MonoBehaviour
                     transform.localScale = new Vector3(-1, 1, 1);
                     patrolDestination = 1;
                 }
+            }
+        }*/
+
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                patrolDestination = 1;
+            }
+        }
+
+        if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                patrolDestination = 0;
             }
         }
 
