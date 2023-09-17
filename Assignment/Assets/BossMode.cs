c9763e486d1ef559a7081d89112993410823d368
Yung729 <leongkahyung@gmail.com>
2023-09-18 00:50:54 +0800

    boss anim

---
+++ Assignment/Assets/BossMode.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMode : StateMachineBehaviour
+{
+    private int rand;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rand = Random.Range(0, 6);
+
+        if (rand == 0)
+        {
+            animator.SetTrigger("Idle");
+        }
+        else if (rand == 1)
+        {
+            animator.SetTrigger("Melee");
+        }
+        else if (rand == 2)
+        {
+            animator.SetTrigger("Attack");
+        }
+        else if (rand == 3)
+        {
+            animator.SetTrigger("Laser");
+        }
+        else if (rand == 4)
+        {
+            animator.SetTrigger("Defend");
+        }
+        else if (rand == 5)
+        {
+            animator.SetTrigger("Defend2");
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+}
