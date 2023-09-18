bdd3912a064ea32b751787926dc8c843aab609c4
Yung729 <leongkahyung@gmail.com>
2023-09-18 21:53:06 +0800

    a

--- Assignment/Assets/BossChase.cs
+++ Assignment/Assets/BossChase.cs
@@ -8,10 +8,16 @@ public class BossChase : MonoBehaviour
     public float radius;
 
     private Transform playerTransform;
+    private bool facingRight = true;
+
+    private GameObject hitBox = default;
+    private GameObject hitBox1 = default;
 
     public void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        hitBox = transform.GetChild(0).gameObject;
+        hitBox1 = transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
@@ -24,8 +30,32 @@ public class BossChase : MonoBehaviour
             if (distance < radius)
             {
                 transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
-
+                if ((playerTransform.position.x < transform.position.x && facingRight) ||
+                    (playerTransform.position.x > transform.position.x && !facingRight))
+                {
+                    FlipBossDirection();
+                }
             }
         }
+    }
+
+    private void FlipBossDirection()
+    {
+        facingRight = !facingRight; // Toggle the facing direction
+
+        // Get the current local scale
+        Vector3 scale = transform.localScale;
+        Vector3 scale1 = hitBox.transform.localScale;
+        Vector3 scale2 = hitBox1.transform.localScale;
+
+        // Flip the X-axis of the local scale
+        scale.x *= -1;
+        scale1.x *= -1;
+        scale2.x *= -1;
+
+        // Apply the new local scale to flip the boss
+        transform.localScale = scale;
+        hitBox.transform.localScale = scale1;
+        hitBox1.transform.localScale = scale2;
     }
 }
