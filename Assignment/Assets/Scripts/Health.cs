35f8d971e0e89bf5be117e87c007dcb14454dd6a
Yung729 <leongkahyung@gmail.com>
2023-09-21 22:24:35 +0800

    enemy detect lava death

--- Assignment/Assets/Scripts/Health.cs
+++ Assignment/Assets/Scripts/Health.cs
@@ -9,14 +9,21 @@ public class Health : MonoBehaviour
     public int maxHealth = 10;
     public int currentHealth = 0;
     public Animator anim;
-
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask lavaGround;
     // Start is called before the first frame update
     private void Start()
     {
+        coll = GetComponent<BoxCollider2D>();
         currentHealth = maxHealth;
     }
 
-
+    private void Update() {
+        if (Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, lavaGround))
+        {
+            Damage(maxHealth);
+        }
+    }
 
     public void Damage(int damage)
     {
