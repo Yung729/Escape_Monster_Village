c514de0a5ccd44b7e1ecda2c3b1309a18b84a071
Yung729 <leongkahyung@gmail.com>
2023-09-25 14:05:50 +0800

    a

--- Assignment/Assets/Scripts/ItemCollecter.cs
+++ Assignment/Assets/Scripts/ItemCollecter.cs
@@ -12,11 +12,14 @@ public class ItemCollecter : MonoBehaviour
     [SerializeField] private AudioSource damageSound;
     [SerializeField] private AudioSource speedSound;
 
+    private GameObject buff1 = default;
+
     private void OnTriggerEnter2D(Collider2D collision) {
 
         if (collision.gameObject.CompareTag("Potions"))
         {
             StartCoroutine(VisualIndicator(Color.green));
+            StartCoroutine(bufferEffect(1));
             Destroy(collision.gameObject);
             player.addHealth(30);
             healthSound.Play();
@@ -26,6 +29,7 @@ public class ItemCollecter : MonoBehaviour
         if (collision.gameObject.CompareTag("Potions1"))
         {
             StartCoroutine(VisualIndicator(Color.red));
+            StartCoroutine(bufferEffect(2));
             Destroy(collision.gameObject);
             damage.increaseDamage(2);
             damageSound.Play();
@@ -34,6 +38,7 @@ public class ItemCollecter : MonoBehaviour
         if (collision.gameObject.CompareTag("Potions2"))
         {
             StartCoroutine(VisualIndicator(Color.blue));
+            StartCoroutine(bufferEffect(3));
             Destroy(collision.gameObject);
             control.increaseSpeed(2.0F);
             speedSound.Play();
@@ -46,6 +51,14 @@ public class ItemCollecter : MonoBehaviour
         GetComponent<SpriteRenderer>().color = color;
         yield return new WaitForSeconds(0.15f);
         GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private IEnumerator bufferEffect(int index)
+    {
+        buff1 = transform.GetChild(index).gameObject;
+        buff1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        buff1.SetActive(false);
     }
 
 }
